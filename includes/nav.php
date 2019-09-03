09030f1943baf16adde9b95ae3d842491658f2d5
Robbe <26785@ma-web.nl>
2019-09-03 13:35:46 +0200

    First commit
    Added first few files. Basics of the homepage and a start of the css.

---
+++ includes/nav.php
@@ -0,0 +1,10 @@
+<nav>
+    <div class="left">
+        <h2 class="left_title">< Robbe /></h2>
+    </div>
+    <div class="right">
+        <a href="#who" class="right__a">About me</a>
+        <a href="#skills" class="right__a">Skills</a>
+        <a href="#contact" class="right__a">Contact</a>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php require 'includes/meta.php'; ?>
+</head>
+<body>
+    <?php require 'includes/nav.php'; ?>
+    <div class="wrapper">
+        <div class="titles">
+            <h1 class="titles__title">Robbe van der Lee</h1>
+            <h2 class="titles__subtitle">Fullstack media developer</h2>
+        </div>
+        <div class="who" id="who">
+            <h2 class="who__title">Who am I?</h2>
+            <p class="who__p">My name is Robbe, Robbe van der Lee.
+                I am currently studying to become a fullstack webdeveloper
+                at the Mediacollege in Amsterdam. When I am not at school I           
+                usually work as a barista for Starbucks.
+            </p>
+        </div>
+        <div class="skills" id="skills">
+            <h2 class="skills__title">Skills</h2>
+            <p class="skills__p">Everyday I work to become better at the languages I use.
+                For frontend I am currently working a lot with HTML5, CSS3 and Javascript for school.
+                In my own time I am working on my <!-- Hier later invoegen -->
+                For backend I mainly work with PHP and SQL, but I am always looking for new skills to learn.
+                if you want to stay updated on my work and current projects, take a look at my github <a href="https://github.com/RobbeLee" target="_blank">RobbeLee</a>
+            </p>
+        </div>
+        <div class="contact" id="contact">
+            <h2 class="contact__title">Contact</h2>
+            <p class="contact__p"> There are several ways to contact me and/or find me.</p>
+            <ul class="contact__list">
+                <li class="contact__list__li">Email : robbevanderlee@gmail.com</li>
+                <li class="contact__list__li">Phone : +31 06 12 55 91 73</li>
+                <li class="contact__list__li">LinkedIn : <a href="https://www.linkedin.com/in/robbe-lee/" target="_blank" class="contact__list__li__a">Robbe van der Lee</a></li>
+                <li class="contact__list__li">Github : <a href="https://github.com/RobbeLee" target="_blank" class="contact__list__li__a">RobbeLee</a></li>
+                <li class="contact__list__li">Or talk to me in real life :)</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
